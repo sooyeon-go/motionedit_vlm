bf5e7007dfae069a77f2c65a6bc3e69010af745c
sooyeon-go <gojjang385@naver.com>
2026-06-18 20:02:31 +0900

    initial commit

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 python inference/progressive_pose_edit.py \
-  --source_image path/to/source.png \
-  --target_image path/to/target.png \
+  --source_image /data/project-vilab/sy/qwen/VQA_edit/image_dataset/cat_1.png \
+  --target_image /data/project-vilab/sy/qwen/VQA_edit/image_dataset/cat_3.png \
   --output_dir outputs/progressive_pose_edit/demo
